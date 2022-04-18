dcf3e0192adcdfef0140cc3903c2b3b96a07d8da
AlsuGibadullina <79659600+AlsuGibadullina@users.noreply.github.com>
2022-04-18 17:58:01 +0300

    вывод новостей и приютов

--- src/app/pages/news/News.tsx
+++ src/app/pages/news/News.tsx
@@ -5,9 +5,27 @@ import {Footer} from "../../components/footer/Footer";
 import {NewsCard} from "../../components/newsCard/NewsCard";
 import news1 from "../../../assets/news1.png"
 import "./News.sass";
+import {collection, getDocs} from "firebase/firestore";
+import {database} from "../../config/firebase";
+import {useEffect, useState} from "react";
 
 export const News = () => {
-    return(
+
+    const newsDatabaseRef = collection(database, 'news');
+    const [newsInfo, setNewInfo] = useState<any>([]);
+
+    useEffect(() => {
+
+        const getNewsInfo = async () => {
+            const data = await getDocs(newsDatabaseRef);
+            let arr = data.docs.map((doc) => ({...doc.data()}));
+            console.log(newsInfo);
+            setNewInfo(arr);
+        };
+        getNewsInfo().then(err => console.log(err));
+    })
+
+    return (
         <div>
             <div className="news-page-body">
                 <div className="news-wrapper">
@@ -18,17 +36,15 @@ export const News = () => {
                     <div className="news-list">
                         <NewsCard title={"Зооволонтеры в наше время: кто бескорыстно борется за жизнь животных"}
                                   description={"Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint." +
-                                  " Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet."}
+                                      " Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet."}
                                   date={"30 июля 2018"} image={news1}/>
-                        <NewsCard title={"Помочь братьям меньшим: пять волонтерских программ для животных"}
-                                  description={"Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint." +
-                                  " Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet."}
-                                  date={"8 ноября 2018"} image={news1}/>
-                        <NewsCard title={"Зачем помогать приютам для домашних животных и как это сделать"}
-                                  description={"Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint." +
-                                  " Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet."}
-                                  date={"30 июля 2018"} image={news1}/>
-
+                        {newsInfo.map((newInfo: any) => (
+                                <NewsCard title={newInfo.title}
+                                          description={newInfo.description}
+                                          date={newInfo.date}
+                                          image={""}/>
+                            )
+                        )}
                     </div>
                 </div>
 
--- src/app/pages/profile/Profile.tsx
+++ src/app/pages/profile/Profile.tsx
@@ -102,6 +102,7 @@ export const Profile = observer(() =>{
                         <p>
                             {usersInfo.phone}
                         </p>
+                        q
                         <p>
                             {usersInfo.email}
                         </p>
