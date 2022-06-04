c7a8457b722207c8a19de91aec4e9a628083f00e
Ruslan-Ziyangirov <79520398+Ruslan-Ziyangirov@users.noreply.github.com>
2022-06-04 19:04:41 +0300

    fixed footer

--- src/app/pages/profile/Profile.tsx
+++ src/app/pages/profile/Profile.tsx
@@ -1,29 +1,32 @@
 import {useEffect, useState} from "react";
 import {useNavigate} from "react-router-dom";
-import {database, logOut, useAuth} from "../../config/firebase";
+import {database, logOut, storage, useAuth} from "../../config/firebase";
 import "./Profile.sass"
 import {ButtonMedium} from "../../components/ui/buttons/medium/ButtonMedium";
 import {VisitCard} from "../../components/cards/visitCard/VisitCard";
-import {collection, doc, getDocs} from "firebase/firestore";
+import {collection, doc, getDocs, updateDoc} from "firebase/firestore";
 import {observer} from "mobx-react";
-import {getAuth, onAuthStateChanged} from "@firebase/auth";
+import {getAuth, onAuthStateChanged, updateProfile} from "@firebase/auth";
 import {toast} from "react-toastify";
 import {SkeletonProfilePage} from "../../components/aimation/skeleton/skeletonProfilePage/SkeletonProfilePage";
 import "firebase/compat/storage";
+import {getDownloadURL, ref, uploadBytes} from "firebase/storage";
+import {Footer} from "../../components/footer/Footer";
 
 
 export const Profile = observer(() =>{
 
+    const profileDocRef = doc(database, 'profile', "IRP8twdILbz5XYIJ3gmc");
     const usersDatabaseRef = collection(database, 'profile');
     const historyDatabaseRef = collection(database, 'history');
+
     const currentUser = useAuth()
     let uid = "";
     let router = useNavigate()
     const auth = getAuth();
 
-    const [file, setFile] = useState(null);
-    const [url, setURL] = useState("");
-
+    const [photo, setPhoto] = useState(null)
+    const [photoURL, setPhotoURL] = useState(currentUser?.photoURL)
     const [loading, setLoading] = useState(false);
     const [usersInfo, setUsersInfo] = useState<any>([]);
     const [visitHistory, setVisitHistory] = useState<any>([]);
@@ -35,6 +38,20 @@ export const Profile = observer(() =>{
 
         }
     });
+
+    function handleChange(e: any) {
+        if (e.target.files[0]) {
+            setPhoto(e.target.files[0])
+        }
+        console.log(photo)
+    }
+
+    function handleClick() {
+        updateDoc(profileDocRef, {
+            "photoURL": "https://sun9-west.userapi.com/sun9-61/s/v1/ig2/RT9stY8SZrahFFo0sPTGpdY_GRb2D5dRI4OCAOP6yral0UQEkGqv4szvq2IIv2V9jDX9gJinv4ECY_bnrkBcA0Pa.jpg?size=1080x1080&quality=95&type=album"
+        });
+        window.location.reload()
+    }
 
     async function handleLogout(){
         setLoading(true)
@@ -73,11 +90,6 @@ export const Profile = observer(() =>{
         }, 2000);
         return () => clearTimeout(timing);
     }, []);
-
-
-    const profileDocRef = doc(database, 'profile', "IRP8twdILbz5XYIJ3gmc");
-
-
 
 
     useEffect(() => {
@@ -126,10 +138,11 @@ export const Profile = observer(() =>{
                                 {usersInfo.email}
                             </p>
                             <form>
-                                <input type="file" className="upload-file" multiple={true} />
+                                <input type="file" className="upload-file" multiple={true} onChange={handleChange} />
                                 <ButtonMedium title={"Поменять фото"}
                                               background={"#713EDD"}
                                               color={"white"}
+                                              onClick={handleClick}
                                 />
                             </form>
                             <button disabled={loading || !currentUser}
@@ -147,6 +160,7 @@ export const Profile = observer(() =>{
 
                         </div>
                     </div>
+
                 </>
             }
         </div>
